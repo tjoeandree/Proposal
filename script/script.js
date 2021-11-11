10d8082f46951b727aa6fc699050a65ff19a9df9
tjoeandree <tjoeandree@gmail.com>
2021-11-11 15:19:38 +0700

    tambahin section pricing

--- script/script.js
+++ script/script.js
@@ -12,13 +12,13 @@ let button = document.getElementById('buttonPrice');
 
 let string1 = document.getElementById('harga1').innerHTML.split('.').join('');
 let string2 = document.getElementById('harga2').innerHTML.split('.').join('');
-let string3 = document.getElementById('harga3').innerHTML.split('.').join('');
+// let string3 = document.getElementById('harga3').innerHTML.split('.').join('');
 
 function getTotalandDP() {
   var num1 = string1.replace(/[^0-9]+/g, '');
   var num2 = string2.replace(/[^0-9]+/g, '');
-  var num3 = string3.replace(/[^0-9]+/g, '');
-  let total = +num1 + +num2 + +num3;
+  // var num3 = string3.replace(/[^0-9]+/g, '');
+  let total = +num1 + +num2;
   let DP = total / 2;
   document.getElementById('total').innerHTML += total.toLocaleString().replace(/,/g, '.');
   document.getElementById('DP').innerHTML += DP.toLocaleString().replace(/,/g, '.');

69d96d3ffba5b59a81bd3ccd5b10ad910f70bee8
tjoeandree <tjoeandree@gmail.com>
2021-11-12 09:43:24 +0700

    masukin fitur scroll pake mousewheel dan fitur nonaktifin zoom untuk mobile

--- script/script.js
+++ script/script.js
@@ -25,3 +25,22 @@ function getTotalandDP() {
 }
 
 window.onload = getTotalandDP;
+
+(function () {
+  function scrollHorizontally(e) {
+    e = window.event || e;
+    var delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
+    document.documentElement.scrollLeft -= delta * 40; // Multiplied by 40
+    document.body.scrollLeft -= delta * 40; // Multiplied by 40
+    e.preventDefault();
+  }
+  if (window.addEventListener) {
+    // IE9, Chrome, Safari, Opera
+    window.addEventListener('mousewheel', scrollHorizontally, false);
+    // Firefox
+    window.addEventListener('DOMMouseScroll', scrollHorizontally, false);
+  } else {
+    // IE 6/7/8
+    window.attachEvent('onmousewheel', scrollHorizontally);
+  }
+})();

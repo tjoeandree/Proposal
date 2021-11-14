72455d6b0a8603f9bf103871f3542a17a5bfcf26
tjoeandree <tjoeandree@gmail.com>
2021-11-14 11:35:41 +0700

    kasih hover:active

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// Efek Rubah Background
 function bgChange() {
   if (window.scrollX > window.innerWidth / 1.2) {
     document.body.classList.add('bg-change');
@@ -8,16 +9,16 @@ function bgChange() {
 
 window.addEventListener('scroll', bgChange);
 
+// Fungsi Jumlah Harga Produk dan Estimasi DP
 let button = document.getElementById('buttonPrice');
 
 let string1 = document.getElementById('harga1').innerHTML.split('.').join('');
 let string2 = document.getElementById('harga2').innerHTML.split('.').join('');
-// let string3 = document.getElementById('harga3').innerHTML.split('.').join('');
 
 function getTotalandDP() {
   var num1 = string1.replace(/[^0-9]+/g, '');
   var num2 = string2.replace(/[^0-9]+/g, '');
-  // var num3 = string3.replace(/[^0-9]+/g, '');
+
   let total = +num1 + +num2;
   let DP = total / 2;
   document.getElementById('total').innerHTML += ' ' + total.toLocaleString().replace(/,/g, '.');
@@ -26,6 +27,7 @@ function getTotalandDP() {
 
 window.onload = getTotalandDP;
 
+// Fungsi horisontal scroll dengan mousewheel
 (function () {
   function scrollHorizontally(e) {
     e = window.event || e;
